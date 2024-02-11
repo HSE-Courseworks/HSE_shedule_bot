1b36f30b7a3d52fd09083a03067e24ffa9b679ad
ermolkina77 <adermolina@edu.hse.ru>
2024-02-11 17:44:57 +0300

    make folders, send table by url in telegram

---
+++ BotScripts/bot.py
@@ -0,0 +1,26 @@
+import telebot
+from TableScripts.download_google import download_sheet
+
+
+
+class MyTelegramBot:
+    def __init__(self, token):
+        self.bot = telebot.TeleBot(token)
+
+        @self.bot.message_handler(commands=['get_sheet'])
+        def handle_get_sheet(message):
+            self.bot.send_message(message.chat.id, "Привет! Я бот. Отправь мне ссылку на таблицу")
+
+        @self.bot.message_handler(func=lambda message: True)
+        def handle_get_sheet(message):
+            if 'docs.google.com/spreadsheets' in message.text:
+                sheet_link = message.text
+                sheet_content = download_sheet(sheet_link)
+                with open(sheet_content, 'rb') as document:
+                    self.bot.send_document(message.chat.id, document)
+            else:
+                self.bot.send_message(message.chat.id, "Пожалуйста, отправьте действительную ссылку на Google таблицу.")
+
+    def run(self):
+        self.bot.polling(none_stop=True)
+
---
+++ main.py
@@ -0,0 +1,6 @@
+from BotScripts.bot import MyTelegramBot
+
+if __name__ == "__main__":
+    bot = MyTelegramBot('token')
+    bot.run()
+
